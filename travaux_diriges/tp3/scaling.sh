7ed554916e23404e6d2fed1e6b2478bc180b3a61
kartofelr <johnmepno@gmail.com>
2026-02-04 11:18:12 +0100

    scaling graphs

--- travaux_diriges/tp3/scaling.sh
+++ travaux_diriges/tp3/scaling.sh
@@ -1,5 +1,6 @@
 echo "cores,time" > results.csv
-for p in {2..16}
+for p in {1..16}
 do
+    echo "working with $p processors"
     mpiexec --oversubscribe -np $p python bucket.py >> results.csv
 done
